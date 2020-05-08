0c6bfd0993a3e9392397a64c40f0e553640f2410
mo mac <trrl.mo@hotmail.com>
2020-05-08 08:52:55 -0600

    Added Route Selection by Airline

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Table from './components/Table';
 
 class App extends Component {
   state = {
-    routes: this.limitedRoutes(),
+    routes: RouteData.routes,
     startingRouteIdx: 0,
     routeLimit: 25,
   };
@@ -19,19 +19,40 @@ class App extends Component {
     }
   };
 
-  limitedRoutes(startingRouteIdx=0, routeLimit=25) {
+  limitedRoutes(routes, startingRouteIdx=0, routeLimit=25) {
     const endingRouteIdx = startingRouteIdx + routeLimit;
-    return RouteData.routes.slice(startingRouteIdx, endingRouteIdx);
+    return routes.slice(startingRouteIdx, endingRouteIdx);
+  };
+
+  filterRoutesByAirline = (selectedAirlineName) => {
+    if (selectedAirlineName === 'all') { return RouteData.routes }
+
+    const selectedAirlineId = this.findAirlineIdFromName(selectedAirlineName);
+
+    return RouteData.routes.filter((route) => {
+      return route.airline === selectedAirlineId;
+    });
+  };
+
+  findAirlineIdFromName = (selectedAirlineName) => {
+    return RouteData.airlines.find((airline) => {
+      return airline.name === selectedAirlineName
+    }).id;
   };
 
   totalRoutes = () => {
-    return RouteData.routes.length;
+    return this.state.routes.length;
+  };
+
+  handleAirlineSelection = (e) => {
+    const selectedAirline = e.target.value;
+    const selectedRoutes = this.filterRoutesByAirline(selectedAirline);
+    this.setState({ routes: selectedRoutes });
   };
 
   handlePreviousClick = (_) => {
     const newStartingIdx = this.state.startingRouteIdx - this.state.routeLimit;
     this.setState({
-      routes: this.limitedRoutes(newStartingIdx, this.state.routeLimit),
       startingRouteIdx: newStartingIdx,
     });
   };
@@ -39,7 +60,6 @@ class App extends Component {
   handleNextClick = (_) => {
     const newStartingIdx = this.state.startingRouteIdx + this.state.routeLimit;
     this.setState({
-      routes: this.limitedRoutes(newStartingIdx, this.state.routeLimit),
       startingRouteIdx: newStartingIdx,
     });
   };
@@ -57,19 +77,34 @@ class App extends Component {
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
+          <form>
+            <select onChange={this.handleAirlineSelection}>
+              <option key='all' value='all'>All Airlines</option>
+              {RouteData.airlines.map((airline, idx) => {
+                return <option 
+                         key={airline.name}
+                         value={airline.name}
+                       >
+                         {airline.name}
+                       </option>
+              })}
+            </select>
+          </form>
           <Table
             columns={columns}
-            rows={this.state.routes}
+            rows={this.limitedRoutes(this.state.routes, this.startingRouteIdx, this.routeLimit)}
             format={this.formatValue}
           />
-          <p>Showing {this.state.startingRouteIdx}-{this.state.startingRouteIdx + this.state.routeLimit} of {this.totalRoutes()} routes.</p>
+          <p>Showing {this.state.startingRouteIdx}-{this.state.startingRouteIdx +
+                      this.state.routeLimit} of {this.totalRoutes()} routes.</p>
           <div>
             <button onClick={this.handlePreviousClick}
                     disabled={(this.state.startingRouteIdx - this.state.routeLimit) < 0}>
               Previous Page
             </button>
             <button onClick={this.handleNextClick}
-                    disabled={this.state.startingRouteIdx + this.state.routeLimit  >= this.totalRoutes()}>
+                    disabled={this.state.startingRouteIdx +
+                              this.state.routeLimit  >= this.totalRoutes()}>
               Next Page
             </button>
           </div>
